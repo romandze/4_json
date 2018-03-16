935a52a3a1a96973d7b7bcb06e2aadcc69837021
Roman <ivastapas@gmail.com>
2018-03-16 13:18:31 +0300

    Рефакторинг кода, и добавил реакции на ошибки

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,26 @@
 import json
 import sys
 
-filepath = sys.argv[1]
-
-def load_data(filepath):
-    with open(filepath, 'r') as f:
-        json_data = json.loads(f.read())
-    return json_data
+def load_data():
+    if len(sys.argv) > 1:
+        try:
+            filepath = sys.argv[1]
+            with open(filepath, 'r') as open_file:
+                json_data = json.loads(open_file.read())
+            return json_data
+        except IOError:
+            print("А нет такого файла")
+            sys.exit()
+    else:
+        print("Ты забыл указать файл JSON")
+        sys.exit()
 
 def pretty_print_json(data):
-    return print (json.dumps(data, sort_keys=True, indent=4))
-
+    pretty_json = json.dumps(data, sort_keys=True, indent=4)
+    return pretty_json
 
 if __name__ == '__main__':
-    print(pretty_print_json(load_data('alco_shops.json')))
+    try:
+        print(pretty_print_json(load_data()))
+    except json.decoder.JSONDecodeError:
+        print("Это был не JSON")
