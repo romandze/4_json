3e73d40b746c4ed300ed5431fa2e4b193ade7c83
Roman <ivastapas@gmail.com>
2018-03-17 14:08:35 +0300

    вынес часть кода и функции main

--- pprint_json.py
+++ pprint_json.py
@@ -1,27 +1,24 @@
 import json
 import sys
 
-
-def load_data(filepatch):
-            with open(filepath, 'r') as open_file:
-                json_data = json.loads(open_file.read())
-            return json_data
-
-
-def pretty_generate_json(data_from_file):
-    pretty_json = json.dumps(data_from_file, sort_keys=True, indent=4)
-    return pretty_json
-
-
-if __name__ == '__main__':
+def load_data():
     if len(sys.argv) > 1:
         try:
             filepath = sys.argv[1]
-            try:
-                print(pretty_generate_json(load_data(filepath)))
-            except json.decoder.JSONDecodeError:
-                print('Это был не JSON')
+            with open(filepath, 'r') as open_file:
+                json_data = json.loads(open_file.read())
+            return json_data
         except IOError:
-            print('А нет такого файла')
+            print("А нет такого файла")
     else:
-        print('Ты забыл указать файл JSON')
+        print("Ты забыл указать файл JSON")
+
+def pretty_print_json(data_from_file):
+    pretty_json = json.dumps(data_from_file, sort_keys=True, indent=4)
+    return pretty_json
+
+if __name__ == '__main__':
+    try:
+        print(pretty_print_json(load_data()))
+    except json.decoder.JSONDecodeError:
+        print("Это был не JSON")
